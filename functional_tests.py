a6048925d485f477e2251d30c7bb0de5b79be5b0
Arnold Taremwa <arnoldo@arnoldo.local>
2017-10-15 20:56:30 +0300

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import pytest
 
 @pytest.fixture(scope="module")
@@ -24,18 +26,29 @@ def test__can_start_a_list_and_retrieve_it_later(browser):
 
     # She notices the page title and header mention to-do lists
     assert 'To-Do' in browser.title
-    assert "Finish this test!" is None
+    header_text = browser.find_element_by_tag_name('h1').text
+    assert 'To-Do' in header_text
 
     # She is invited to enter a to-do item straight away
+    inputbox = browser.find_element_by_id('id_new_item')
+    assert inputbox.get_attribute('placeholder') == 'Enter a to-do item'
 
     # She types "Buy peacock feathers" into a text box (Edith's hobby
     # is tying fly-fishing lures)
+    inputbox.send_keys('Buy peacock feathers')
 
     # When she hits enter, the page updates, and now the page lists
     # "1: Buy peacock feathers" as an item in a to-do list
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
+
+    table = browser.find_element_by_id('id_list_table')
+    rows = table.find_element_by_tag_name('tr')
+    assert any(row.text == '1: Buy peacock feathers' for row in rows)
 
     # There is still a text box inviting her to add another item.
     # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+    assert 'Finish this test' is None
 
     # The page updates again, and now shows both items on her list
 
@@ -46,5 +59,3 @@ def test__can_start_a_list_and_retrieve_it_later(browser):
     # She visits that URL - her to-do list is still there.
 
     # Satisfied, she goes back to sleep
-
-    browser.quit()
