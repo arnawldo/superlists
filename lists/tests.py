d34698c6889398d7d0eaba5f1817f3e11d6fa84e
Arnold Taremwa <arnoldo@arnoldo.local>
2017-10-15 20:19:06 +0300

    First unit test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,2 +1,6 @@
-def test__bad_math():
-    assert 1 + 1 == 3
+from django.urls import resolve
+from lists.views import home_page
+
+def test__root_url_resolves_to_home_page_view():
+    found = resolve('/')
+    assert found.func is home_page
