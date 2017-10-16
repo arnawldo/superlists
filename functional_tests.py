9cb9115086b80cf3c10a1eafdfb4065bed209401
Arnold Taremwa <arnoldo@arnoldo.local>
2017-10-16 15:01:48 +0300

    Can receive POST and render data. Tests for table currently fail

--- functional_tests.py
+++ functional_tests.py
@@ -43,19 +43,27 @@ def test__can_start_a_list_and_retrieve_it_later(browser):
     time.sleep(1)
 
     table = browser.find_element_by_id('id_list_table')
-    rows = table.find_element_by_tag_name('tr')
-    assert any(row.text == '1: Buy peacock feathers' for row in rows)
+    rows = table.find_elements_by_tag_name('tr')
+    assert '1: Buy peacock feathers' in [row.text for row in rows]
 
     # There is still a text box inviting her to add another item.
     # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
-    assert 'Finish this test' is None
+    inputbox = browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Use peacock feathers to make a fly')
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
 
     # The page updates again, and now shows both items on her list
+    table = browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    assert '1: Buy peacock feathers' in [row.text for row in rows]
+    assert '2: Use peacock feathers to make a fly' in [row.text for row in rows]
 
     # Edith wonders whether the site will rememmber her list. Then she sees
     # that the site has generated a unique URL for her -- there is some
     # explanatory text to that effect.
-
+    assert 'Finish this test' is None
+    
     # She visits that URL - her to-do list is still there.
 
     # Satisfied, she goes back to sleep
--- lists/tests.py
+++ lists/tests.py
@@ -7,3 +7,7 @@ def test__uses_home_template(client):
     response = client.get('/')
     # check what template was used to render a response
     assert 'home.html' in [t.name for t in response.templates]
+
+def test__can_save_a_POST_request(client):
+    response = client.post('/', data={'item_text': 'A new list item'})
+    assert 'A new list item' in response.content.decode()
