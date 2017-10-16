83dba93e329ee4d3822ea674418b485ec9c2ed37
Arnold Taremwa <arnoldo@arnoldo.local>
2017-10-16 15:12:35 +0300

    Moved repetitve checks into function

--- functional_tests.py
+++ functional_tests.py
@@ -18,6 +18,10 @@ def browser(request):
 
     return firefox_browser
 
+def check_for_row_in_list_table(browser, row_text):
+    table = browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    assert row_text in [row.text for row in rows]
 
 def test__can_start_a_list_and_retrieve_it_later(browser):
     # Edith has heard about a cool new online to-do app. 
@@ -42,9 +46,7 @@ def test__can_start_a_list_and_retrieve_it_later(browser):
     inputbox.send_keys(Keys.ENTER)
     time.sleep(1)
 
-    table = browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    assert '1: Buy peacock feathers' in [row.text for row in rows]
+    check_for_row_in_list_table(browser, '1: Buy peacock feathers')
 
     # There is still a text box inviting her to add another item.
     # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -54,16 +56,14 @@ def test__can_start_a_list_and_retrieve_it_later(browser):
     time.sleep(1)
 
     # The page updates again, and now shows both items on her list
-    table = browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    assert '1: Buy peacock feathers' in [row.text for row in rows]
-    assert '2: Use peacock feathers to make a fly' in [row.text for row in rows]
+    check_for_row_in_list_table(browser, '1: Buy peacock feathers')
+    check_for_row_in_list_table(browser, '2: Use peacock feathers to make a fly')
 
     # Edith wonders whether the site will rememmber her list. Then she sees
     # that the site has generated a unique URL for her -- there is some
     # explanatory text to that effect.
     assert 'Finish this test' is None
-    
+
     # She visits that URL - her to-do list is still there.
 
     # Satisfied, she goes back to sleep
