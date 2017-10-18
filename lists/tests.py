fde2932f6f166d6a59471b36dd2e6eb23b33ccf7
Arnold Taremwa <taremwaarnold@gmail.com>
2017-10-18 09:45:21 +0300

    Add List model and migrations

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.urls import resolve
 from django.http import HttpRequest
 
-from lists.models import Item
+from lists.models import Item, List
 
 import pytest
 
@@ -11,23 +11,33 @@ def test__uses_home_template(client):
     # check what template was used to render a response
     assert 'home.html' in [t.name for t in response.templates]
 
+# ITEMS AND LIST MODELS
 @pytest.mark.django_db
 def test__saving_and_retrieving_items():
+    list_ = List()
+    list_.save()
+
     first_item = Item()
     first_item.text = 'The first (ever) list item'
+    first_item.list = list_
     first_item.save()
 
     second_item = Item()
     second_item.text = 'Item the second'
+    second_item.list = list_
     second_item.save()
 
+    saved_list = List.objects.first()
+    assert saved_list == list_
     saved_items = Item.objects.all()
     assert saved_items.count() == 2
 
     first_saved_item = saved_items[0]
     second_saved_item = saved_items[1]
     assert first_saved_item.text == 'The first (ever) list item'
+    assert first_saved_item.list == list_
     assert second_saved_item.text == 'Item the second'
+    assert second_saved_item.list == list_
 
 @pytest.mark.django_db
 def test__only_saves_items_when_necessary(client):
@@ -41,8 +51,9 @@ def test__uses_list_template(client):
 
 @pytest.mark.django_db
 def test__display_all_items(client):
-    Item.objects.create(text='itemey 1')
-    Item.objects.create(text='itemey 2')
+    list_ = List.objects.create()
+    Item.objects.create(text='itemey 1', list=list_)
+    Item.objects.create(text='itemey 2', list=list_)
 
     response = client.get('/lists/the-only-list-in-the-world/')
 
